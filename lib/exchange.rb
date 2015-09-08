3038598021494a75d85d7b8ed8ff5c26349480bb
Cb-James <james@coinbase.com>
2015-09-08 12:53:04 -0700

    Initial commit

---
+++ lib/exchange.rb
@@ -0,0 +1,47 @@
+# Extended functionality beyond the official Coinbase Exchange
+# wrapper @ https://github.com/coinbase/coinbase-exchange-ruby
+class Exchange
+  def self.account_id(trader, currency)
+    trader.accounts do |resp|
+      @record = resp.select { |account| account.currency == currency }[0]
+    end
+    @record['id']
+  end
+
+  def self.holds_balance(trader, currency, balance = BigDecimal('0'))
+    account_id = self.account_id(trader, currency)
+    trader.account_holds(account_id) do |resp|
+      balance = resp.flat_map { |hold| BigDecimal(hold.amount) }.reduce(:+) if resp.count > 0
+    end
+    balance
+  end
+
+  def self.balance(trader, currency, balance = BigDecimal('0'))
+    trader.accounts do |resp|
+      @record = resp.select { |account| account.currency == currency }[0]
+    end
+    balance = BigDecimal(@record['balance']) - self.holds_balance(trader, currency)
+  end
+
+  def self.available_btc(book)
+    book['asks'].flat_map { |ask| ask[1].to_f }.reduce(:+)
+  end
+
+  def self.max_bid_price(book)
+    book['bids'].flat_map { |bid| bid[0].to_f }.max
+  end
+
+  def self.replenish_btc(trader, btc_amount)
+    wallet = "95671473-4dda-5264-a654-fc6923e8a334" # Official Sandbox fake BTC wallet       
+    deposit_amount = (btc_amount + BigDecimal('10')).truncate(8).to_s('F')
+    puts sprintf "Depositing %.8f BTC", deposit_amount
+    trader.deposit(wallet, deposit_amount)
+  end
+
+  def self.replenish_usd(trader, usd_amount)
+    wallet = "bcdd4c40-df40-5d76-810c-74aab722b223" # Official Sandbox fake USD wallet
+    deposit_amount = (usd_amount + BigDecimal('1000')).truncate(2).to_s('F')
+    puts sprintf "Depositing $%.2f", deposit_amount
+    trader.deposit(wallet, deposit_amount)
+  end
+end
---
+++ lib/hot_potato.rb
@@ -0,0 +1,47 @@
+require 'coinbase/exchange' # main wrapper
+require_relative 'lib/exchange' # extended functionality
+
+# Sandbox Account #1
+key = ENV['EXCHANGE_SB_ACCESS_KEY']
+secret = ENV['EXCHANGE_SB_API_SECRET']
+pass = ENV['EXCHANGE_SB_PASSPHRASE']
+
+# Sandbox Account #2
+alt_key = ENV['ALT_EXCHANGE_SB_ACCESS_KEY']
+alt_secret = ENV['ALT_EXCHANGE_SB_API_SECRET']
+alt_pass = ENV['ALT_EXCHANGE_SB_PASSPHRASE']
+
+# Alice sells
+alice = Coinbase::Exchange::Client.new(key, secret, pass,
+  api_url: 'https://api-public.sandbox.exchange.coinbase.com')
+
+# Bob buys
+bob = Coinbase::Exchange::Client.new(alt_key, alt_secret, alt_pass,
+  api_url: 'https://api-public.sandbox.exchange.coinbase.com')
+
+while true  # infinite loop!
+  # Suck down the whole order book - either trader's works
+  book = JSON.parse(alice.orderbook(level: 3))
+
+  # Buy/Sell higher than existing orders
+  max_bid_price = Exchange.max_bid_price(book).to_f
+
+  # Use random amounts within a range  
+  sale_price = BigDecimal(rand(max_bid_price + 0.01..max_bid_price + 100).to_s)
+  btc_amount = BigDecimal(rand(1.00..3.00).to_s)
+  usd_amount = sale_price * btc_amount
+
+  # Top up accounts if nessesary
+  Exchange.replenish_btc(alice, btc_amount) if Exchange.balance(alice, 'BTC') < btc_amount
+  Exchange.replenish_usd(bob, usd_amount) if Exchange.balance(bob, 'USD') < usd_amount
+  
+  # Sell!
+  puts sprintf "Selling %.8f BTC @ $%.2f", btc_amount, sale_price
+  alice.sell(btc_amount.round(8), sale_price.round(2))
+  sleep 10
+
+  # Buy!  
+  puts sprintf "Buying %.8f BTC @ $%.2f", btc_amount, sale_price
+  bob.buy(btc_amount.round(8), sale_price.round(2))
+  sleep 10
+end

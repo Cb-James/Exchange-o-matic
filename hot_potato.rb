7b1002da2f522f5bf577a49872a81d5df7baf951
Cb-James <james@coinbase.com>
2015-10-11 17:23:42 -0700

    code cleanup

--- hot_potato.rb
+++ hot_potato.rb
@@ -1,4 +1,4 @@
-# Creates two bots on the Sandbox and buys and sells endlessly between them 
+# Creates two bots on the Sandbox and buys and sells endlessly between them
 require 'coinbase/exchange' # main wrapper
 require_relative 'lib/exchange' # extended functionality
 
@@ -12,36 +12,52 @@ alt_key = ENV['ALT_EXCHANGE_SB_ACCESS_KEY']
 alt_secret = ENV['ALT_EXCHANGE_SB_API_SECRET']
 alt_pass = ENV['ALT_EXCHANGE_SB_PASSPHRASE']
 
-# Alice sells
-alice = Coinbase::Exchange::Client.new(key, secret, pass,
-  api_url: 'https://api-public.sandbox.exchange.coinbase.com')
+API_URL = 'https://api-public.sandbox.exchange.coinbase.com'
 
-# Bob buys
-bob = Coinbase::Exchange::Client.new(alt_key, alt_secret, alt_pass,
-  api_url: 'https://api-public.sandbox.exchange.coinbase.com')
+# Official Sandbox Fake Money Wallets
+wallet = { 'usd' => 'bcdd4c40-df40-5d76-810c-74aab722b223',
+           'btc' => '95671473-4dda-5264-a654-fc6923e8a334' }
 
-while true  # infinite loop!
+# Orderbook info, etc.
+rest_api = Coinbase::Exchange::Client.new(
+  key, secret, pass, api_url: API_URL)
+
+# Alice the bot
+alice = Coinbase::Exchange::Bot.new(
+  key, secret, pass, api_url: API_URL)
+
+# Bob the bot
+bob = Coinbase::Exchange::Bot.new(
+  alt_key, alt_secret, alt_pass, api_url: API_URL)
+
+loop do
   # Buy/Sell higher than existing orders
-  max_bid = Exchange.max_bid(alice).to_f
+  max_bid = rest_api.max_bid.to_f
 
-  # Use random amounts within a range  
+  fee = 1.25
+  # Random amounts make pretty graphs
   sale_price = BigDecimal(rand(max_bid + 0.01..max_bid + 100).to_s)
   btc_amount = BigDecimal(rand(1.00..3.00).to_s)
-  usd_amount = sale_price * btc_amount * BigDecimal('1.25')
+  usd_amount = BigDecimal(sale_price * btc_amount * fee)
 
-  # Top up accounts if nessesary
-  alice.replenish('BTC', btc_amount) if alice.balance('BTC') < btc_amount
-  bob.replenish('USD', usd_amount) if bob.balance('USD') < usd_amount
-  
+  # Top up accounts (plus a little more) if nessesary
+  if alice.balance('BTC') < btc_amount
+    puts format 'Depositing %.8f BTC', btc_amount + 10
+    alice.replenish('BTC', btc_amount + 10, wallet['btc'])
+  end
+
+  if bob.balance('USD') < usd_amount
+    puts format 'Depositing $%.2f', usd_amount + 1000
+    bob.replenish('USD', usd_amount + 1000, wallet['usd'])
+  end
+
   # Sell!
-  puts sprintf "Alice selling %.8f BTC @ $%.2f", btc_amount, sale_price
+  puts format 'Alice selling %.8f BTC @ $%.2f', btc_amount, sale_price
   alice.sell(btc_amount.round(8), sale_price.round(2))
+  alice.wait
 
-  sleep 5
-  
-  # Buy!  
-  puts sprintf "Bob buying %.8f BTC @ $%.2f", btc_amount, sale_price
+  # Buy!
+  puts format 'Bob buying %.8f BTC @ $%.2f', btc_amount, sale_price
   bob.buy(btc_amount.round(8), sale_price.round(2))
-
-  sleep 5
+  bob.wait
 end
--- lib/exchange.rb
+++ lib/exchange.rb
@@ -3,66 +3,64 @@
 
 module Coinbase
   module Exchange
+    # Extensions on the main class
     class Client
+      def max_bid
+        bids = JSON.parse(orderbook(level: 3))['bids']
+        bids.count > 0 ? bids.flat_map { |bid| bid[0].to_f }.max : 0
+      end
+
+      def min_bid
+        bids = JSON.parse(orderbook(level: 3))['bids']
+        bids.count > 0 ? bids.flat_map { |bid| bid[0].to_f }.min : 0
+      end
+
+      def max_ask
+        asks = JSON.parse(orderbook(level: 3))['asks']
+        asks.count > 0 ? asks.flat_map { |bid| bid[0].to_f }.max : 0
+      end
+
+      def min_ask
+        asks = JSON.parse(orderbook(level: 3))['asks']
+        asks.count > 0 ? asks.flat_map { |bid| bid[0].to_f }.min : 0
+      end
+    end
+
+    # Bot-type functionality goes here
+    class Bot < Client
       def account_id(currency)
-        self.accounts do |resp|
+        accounts do |resp|
           @record = resp.select { |account| account.currency == currency }[0]
         end
         @record['id']
       end
 
       def holds_balance(currency, balance = BigDecimal('0'))
-        account_id = self.account_id(currency)
-        self.account_holds(account_id) do |resp|
-          balance = resp.flat_map { |hold| BigDecimal(hold.amount) }.reduce(:+) if resp.count > 0
+        account_id = account_id(currency)
+        account_holds(account_id) do |holds|
+          if holds.count > 0
+            balance = holds.flat_map { |hold| BigDecimal(hold.amount) }.reduce(:+)
+          end
         end
         balance
       end
 
-      def balance(currency, balance = BigDecimal('0'))
-        self.accounts do |resp|
+      def balance(currency, _balance = BigDecimal('0'))
+        accounts do |resp|
           @record = resp.select { |account| account.currency == currency }[0]
         end
-        balance = BigDecimal(@record['balance']) - self.holds_balance(currency)
+        BigDecimal(@record['balance']) - holds_balance(currency)
       end
-    
-      def replenish(currency, amount)
-        case currency
-        when 'BTC'
-          wallet = "95671473-4dda-5264-a654-fc6923e8a334" # Official Sandbox fake BTC wallet
-          deposit_amount = (amount + BigDecimal('10')).truncate(8).to_s('F')
-          puts sprintf "Depositing %.8f BTC", deposit_amount
-        when 'USD'
-          wallet = "bcdd4c40-df40-5d76-810c-74aab722b223" # Official Sandbox fake USD wallet
-          deposit_amount = (amount + BigDecimal('1000')).truncate(2).to_s('F')
-          puts sprintf "Depositing $%.2f", deposit_amount
-        else
-          raise 'Please specify BTC or USD'
-        end
-        self.deposit(wallet, deposit_amount)
+
+      def replenish(currency, amount, wallet)
+        currency == 'BTC' ? precision = 8 : precision = 2
+        deposit_amount = BigDecimal(amount).truncate(precision).to_s('F')
+        deposit(wallet, deposit_amount)
+      end
+
+      def wait
+        sleep 5
       end
     end
-  end
-end
-
-class Exchange
-  def self.max_bid(trader)
-    book = JSON.parse(trader.orderbook(level: 3))
-    book['bids'].count > 0 ? book['bids'].flat_map { |bid| bid[0].to_f }.max : 0
-  end
-
-  def self.min_bid(trader)
-    book = JSON.parse(trader.orderbook(level: 3))
-    book['bids'].count > 0 ? book['bids'].flat_map { |bid| bid[0].to_f }.min : 0
-  end
-
-  def self.max_ask(trader)
-    book = JSON.parse(trader.orderbook(level: 3))
-    book['asks'].count > 0 ? book['asks'].flat_map { |bid| bid[0].to_f }.max : 0
-  end
-
-  def self.min_ask(trader)
-    book = JSON.parse(trader.orderbook(level: 3))
-    book['asks'].count > 0 ? book['asks'].flat_map { |bid| bid[0].to_f }.min : 0
   end
 end
